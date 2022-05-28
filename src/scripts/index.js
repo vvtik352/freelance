5680751461e734c9d7537613b94a819a2442c96e
vvtihomirov <vtihomirov@lintech.ru>
2022-05-28 19:46:33 +0300

    bank slider started

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,31 +1,48 @@
 const leftSliderArrow = document.querySelector('.slider-arrow_left')
 const rightSliderArrow = document.querySelector('.slider-arrow_right')
 
+const bankLeftSliderArrow = document.querySelector('.bank-arrow-left')
+const bankRightSliderArrow = document.querySelector('.bank-arrow-right')
+
 
 const slider = document.querySelector('.slider')
+const bankSlider = document.querySelector('.bank-slider')
 
-function handleRightArrowClick() {
+function handleRightArrowClick(slider, offset) {
     slider.scroll({
-        left: slider.scrollLeft + 350 * 3 + 200,
+        left: slider.scrollLeft + offset,
         behavior: 'smooth'
     })
 
 }
-function handleLeftArrowClick() {
+function handleLeftArrowClick(slider, offset) {
     slider.scroll({
-        left: slider.scrollLeft - 350 * 3 - 200,
+        left: slider.scrollLeft - offset,
         behavior: 'smooth'
     })
+
 }
+
+
 
 function getOffset(el) {
     const rect = el.getBoundingClientRect();
-    console.error(rect);
     return {
         left: rect.x,
         top: rect.top
     };
 }
 
-rightSliderArrow.addEventListener('click', handleRightArrowClick)
-leftSliderArrow.addEventListener('click', handleLeftArrowClick)
+rightSliderArrow.addEventListener('click', () => {
+    handleRightArrowClick(slider, 350)
+})
+leftSliderArrow.addEventListener('click', () => {
+    handleLeftArrowClick(slider, 350)
+})
+
+bankLeftSliderArrow.addEventListener('click', () => {
+    handleLeftArrowClick(bankSlider, 599)
+})
+bankRightSliderArrow.addEventListener('click', () => {
+    handleRightArrowClick(bankSlider, 599)
+})
