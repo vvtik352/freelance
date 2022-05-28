e001ffbc26b3eb5648fed59965f6c3f3d7663b12
vvtihomirov <vtihomirov@lintech.ru>
2022-05-28 17:33:36 +0300

    slide 3 images in one time

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,14 +6,14 @@ const slider = document.querySelector('.slider')
 
 function handleRightArrowClick() {
     slider.scroll({
-        left: slider.scrollLeft + 350,
+        left: slider.scrollLeft + 350 * 3 + 200,
         behavior: 'smooth'
     })
 
 }
 function handleLeftArrowClick() {
     slider.scroll({
-        left: slider.scrollLeft - 350,
+        left: slider.scrollLeft - 350 * 3 - 200,
         behavior: 'smooth'
     })
 }
