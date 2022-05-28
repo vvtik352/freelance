fcbe330d2ce888ed0a521e051dec78a61b9fe59c
vvtihomirov <vtihomirov@lintech.ru>
2022-05-28 17:23:59 +0300

    chore (index.js): added scroll slider

---
+++ src/scripts/index.js
@@ -0,0 +1,31 @@
+const leftSliderArrow = document.querySelector('.slider-arrow_left')
+const rightSliderArrow = document.querySelector('.slider-arrow_right')
+
+
+const slider = document.querySelector('.slider')
+
+function handleRightArrowClick() {
+    slider.scroll({
+        left: slider.scrollLeft + 400,
+        behavior: 'smooth'
+    })
+
+}
+function handleLeftArrowClick() {
+    slider.scroll({
+        left: slider.scrollLeft - 400,
+        behavior: 'smooth'
+    })
+}
+
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    console.error(rect);
+    return {
+        left: rect.x,
+        top: rect.top
+    };
+}
+
+rightSliderArrow.addEventListener('click', handleRightArrowClick)
+leftSliderArrow.addEventListener('click', handleLeftArrowClick)
